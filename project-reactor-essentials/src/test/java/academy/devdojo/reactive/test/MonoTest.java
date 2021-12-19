067e1eb4a2f7ed54ef94867192438f56dfdba008
Daniel Souza <danielfelipe.souza@btgpactual.com>
2021-12-19 11:26:45 -0300

    adiciona configurações iniciais

---
+++ project-reactor-essentials/src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -0,0 +1,14 @@
+package academy.devdojo.reactive.test;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MonoTest {
+
+	@Test
+	public void test(){
+		log.info("Everything working as intended");
+	}
+}
