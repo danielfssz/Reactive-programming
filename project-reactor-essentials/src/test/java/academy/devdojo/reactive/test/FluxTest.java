ce1161665bb3ca15a9514b55274741f6a2062941
Daniel Souza <danielfelipe.souza@btgpactual.com>
2021-12-20 07:00:16 -0300

    flux subscribe - hookOnSubscribe - hookOnNext

--- project-reactor-essentials/src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ project-reactor-essentials/src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -7,6 +7,7 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -113,6 +114,39 @@ public class FluxTest {
 			public void onComplete() {
 
 			}
+		});
+
+		log.info("-----------------");
+
+		StepVerifier.create(flux)
+				.expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+				.verifyComplete()
+		;
+	}
+
+	@Test
+	public void fluxSubscribeNumbersNotSoUglyBackpressure() {
+		Flux<Integer> flux = Flux.range(1, 10)
+				.log();
+
+		flux.subscribe(new BaseSubscriber<Integer>() {
+			private int count = 0;
+			private final int requestCount = 2;
+
+			@Override
+			protected void hookOnSubscribe(Subscription subscription) {
+				request(requestCount);
+			}
+
+			@Override
+			protected void hookOnNext(Integer value) {
+				count++;
+				if (count >= 2) {
+					count = 0;
+					request((requestCount));
+				}
+			}
+
 		});
 
 		log.info("-----------------");
